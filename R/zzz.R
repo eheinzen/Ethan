821151d0933e11e751b6559c3505d8b73e6118fc
Ethan Heinzen <Heinzen.Ethan@mayo.edu>
2017-07-12 10:23:50 -0500

    Comment out .onAttach

--- R/zzz.R
+++ R/zzz.R
@@ -4,10 +4,10 @@
 ### Last Modified: Thursday, 16 June 2016 10:00 AM CDT
 ###########################################################################################
 
-.onAttach <- function(libname, pkgname) {
-  packageStartupMessage(paste0("Take note: if you're looking for the most up-to-date version of auc(), ",
-                               "you should look in the Avatar package,\n",
-                               "and if you're looking for the most up-to-date version of summary2(), ",
-                               "you should look in the bbd package."))
-}
+# .onAttach <- function(libname, pkgname) {
+#   packageStartupMessage(paste0("Take note: if you're looking for the most up-to-date version of auc(), ",
+#                                "you should look in the Avatar package,\n",
+#                                "and if you're looking for the most up-to-date version of summary2(), ",
+#                                "you should look in the bbd package."))
+# }
 
